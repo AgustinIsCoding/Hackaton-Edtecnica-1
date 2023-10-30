3aa9e96d4be89cca7c393c5e46e5ebf6d21fcf07
AgustinIsCoding <145371736+AgustinIsCoding@users.noreply.github.com>
2023-10-30 11:46:09 -0400

    Add files via upload

--- hackaton/index.js
+++ hackaton/index.js
@@ -1,32 +1,28 @@
 alert('Bienvenido al sistema de verificaciòn de calidad de productos, para la determinacion del precio final de un lote de productos.');
 let cantidad
 let cont=0;
-    while(cont==0){
+
+while(cont==0){
     cantidad=prompt('Por favor indique la cantidad de productos a verificar') ;
-    if(typeof parseInt(cantidad)=='number'){
+    if(isNaN(cantidad)==false){
         cont=1;
     }else{
-        alert('Por favor ingrese un valor aceptado, en otras palabras un numero entero.')
+        alert('Por favor ingrese un valor aceptado, en otras palabras un número entero.')
     }
 }
-let num=cantidad
-let cont2=0
-let cont3=0
+
 let arr=[]
 let malo=0, defectuoso=0, bueno=0;
 const precio=parseInt(prompt('Ingrese el precio de compra de lote'));
-for(let i=0;i<parseInt(num);i++){
-    cont3=0
-    if(cont2==0){
-        alert('Hay tres niveles de calidad, 0 producto malo, 1 defectuoso y 2 bueno.')
-        cont2=1
-    }
-    while(cont3==0){ 
+alert('Hay tres niveles de calidad, 0 producto malo, 1 defectuoso y 2 bueno.')
+for(let i=0;i<parseInt(cantidad);i++){
+    cont=2
+    while(cont==2){ 
         arr[i]=parseInt(prompt('Por favor ingrese la calidad del producto '+(i+1)))
         if(arr[i]!=0 && arr[i]!=1&& arr[i]!=2){
             alert('Por favor ingrese un dato aceptado')
         }else{
-            cont3=1;
+            cont=0;
         }
     }
     if(arr[i]==1){
